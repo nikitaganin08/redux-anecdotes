dcab446fa33c34e075d5c2e7c5e91974288370bf
ganin.n.a <nikitaganin>
2021-09-27 15:59:00 +0300

    6.4: anecdotes, step2(adding anecdote functionalities)

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -30,7 +30,8 @@ const reducer = (state = initialState, action) => {
                 ...anecdoteToChange, votes : anecdoteToChange.votes + 1
             }
             return state.map(anecdote => anecdote.id === id ? changedAnecdote : anecdote)
-
+        case 'NEW_ANECDOTE':
+            return [...state, action.data]
         default:
             return state
     }
@@ -40,6 +41,17 @@ export const toggleVoteOf = (id) => {
     return {
         type: 'TOGGLE_VOTE',
         data: { id }
+    }
+}
+
+export const createAnecdote = (content) => {
+    return {
+        type: 'NEW_ANECDOTE',
+        data: {
+            content,
+            votes: 0,
+            id: getId()
+        }
     }
 }
 
