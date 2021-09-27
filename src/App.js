996853bda3e87a12d2e351d196da4a942d34b7c4
ganin.n.a <nikitaganin>
2021-09-27 16:04:02 +0300

    6.5*: anecdotes, step3(add sort of anecdotes by votes)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {createAnecdote, toggleVoteOf} from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
-      dispatch(toggleVoteOf(id))
-  }
+    const vote = (id) => {
+        console.log('vote', id)
+        dispatch(toggleVoteOf(id))
+    }
 
-  const addAnecdote = (event) => {
-      event.preventDefault()
-      const content = event.target.anecdote.value
-      event.target.anecdote.value =''
-      dispatch(createAnecdote(content))
-  }
+    const addAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createAnecdote(content))
+    }
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes
+                .sort((anecdote, nextAnecdote) => nextAnecdote.votes - anecdote.votes)
+                .map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
+                        </div>
+                        <div>
+                            has {anecdote.votes}
+                            <button onClick={() => vote(anecdote.id)}>vote</button>
+                        </div>
+                    </div>
+                )}
+            <h2>create new</h2>
+            <form onSubmit={addAnecdote}>
+                <div><input name='anecdote'/></div>
+                <button>create</button>
+            </form>
         </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote'/></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default App
