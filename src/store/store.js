de854c2b1fbd0ade5f6e41e42e77db4153b34b9d
ganin.n.a <nikitaganin>
2021-09-28 12:28:56 +0300

    6.10 Better anecdotes, step8(add notification reducer)

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,16 @@
-import {createStore} from 'redux'
-import reducer from '../reducers/anecdoteReducer'
+import {combineReducers, createStore} from 'redux'
+import anecdoteReducer from '../reducers/anecdoteReducer'
+import notificationReducer from "../reducers/notificationReducer";
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 const store = () => {
-    return createStore(reducer, composeWithDevTools())
+    const reducer = combineReducers({
+        anecdotes: anecdoteReducer,
+        notification: notificationReducer
+    })
+    const store = createStore(reducer, composeWithDevTools())
+    console.log(store.getState())
+    return store
 }
 
 export default store
